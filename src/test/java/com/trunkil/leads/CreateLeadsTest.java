06b245f019c0843220f85d89aca7896bd03bee57
HP <HP@192.168.0.30>
2021-11-13 14:13:35 +0530

    My Project Commit

---
+++ src/test/java/com/trunkil/leads/CreateLeadsTest.java
@@ -0,0 +1,26 @@
+package com.trunkil.leads;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CreateLeadsTest {
+
+	 
+	@Test
+	public void createLeads() {
+		
+Reporter.log("control is in method", true);
+
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
